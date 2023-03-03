2f9d8c2f82387a599d5cb80e5829fa70a160fafa
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-03-03 16:47:11 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection, Service, BigNFTSilder, Subscribe, Title, Category, Filter, NFTCard, Collection, FollowerTab, AudioLive, Slider } from '../components/componentsindex';
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <Title
+         heading="Audio Collection" 
+         paragraph="Discover the most outstanding NFTs in all topics of life."
+       />
+      <AudioLive />
+      <Title
+         heading="New Collection"
+         paragraph="Discover the most outstanding NFTs in all topics of life."
+       />
+      <FollowerTab />
+      {/*<Title
+        heading="Explore NFTs Video"
+        paragraph="Click on play icon and enjoy NFTs video"
+      />*/}
+      <Slider />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  )
+}
+
+export default Home;
