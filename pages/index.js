b49e7d2a0e86d3a3fefd50c159751562d38a0d3c
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-03-04 14:24:27 +0530

    add Brand

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSilder, Subscribe, Title, Category, Filter, NFTCard, Collection, FollowerTab, AudioLive, Slider } from '../components/componentsindex';
+import { HeroSection, Service, BigNFTSilder, Subscribe, Title, Category, Filter, NFTCard, Collection, FollowerTab, AudioLive, Slider, Brand } from '../components/componentsindex';
 
 const Home = () => {
   return (
@@ -38,6 +38,7 @@ const Home = () => {
       />
       <Category />
       <Subscribe />
+      <Brand />
     </div>
   )
 }
