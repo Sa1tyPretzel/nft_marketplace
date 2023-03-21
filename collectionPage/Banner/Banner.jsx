b77b0b31c451f9ade37d7e9c934e545136c540e3
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-03-21 23:10:03 +0530

    add author page

--- collectionPage/Banner/Banner.jsx
+++ collectionPage/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = ({ bannerImage }) => {
           src={bannerImage}
           objectFit="cover"
           alt="background"
-          width={1600}
+          width={2000}
           height={100}
         />
       </div>
---
+++ pages/author.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useContext } from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/author.module.css";
+import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
+import { Brand, Title } from "../components/componentsindex";
+import FollowerTabCard from "../components/FollowerTab/FollowerTabCard/FollowerTabCard";
+import images from "../img";
+import {
+  AuthorProfileCard,
+  AuthorTaps,
+  AuthorNFTCardBox,
+} from "../authorPage/componentIndex";
+
+const author = () => {
+  const followerArray = [
+    images.user1,
+    images.user2,
+    images.user3,
+    images.user4,
+    images.user5,
+    images.user6,
+    images.user7,
+    images.user8,
+  ]
+
+  const [collectiables, setCollectiables] = useState(true);
+  const [created, setCreated] = useState(false);
+  const [like, setLike] = useState(false);
+  const [follower, setFollower] = useState(false);
+  const [following, setFollowing] = useState(false);
+
+  return (
+    <div className={Style.author}>
+      <Banner bannerImage={images.creatorbackground2} />
+      <AuthorProfileCard />
+      <AuthorTaps
+        setCollectiables={setCollectiables}
+        setCreated={setCreated}
+        setLike={setLike}
+        setFollower={setFollower}
+        setFollowing={setFollowing}
+        //currentAccount={currentAccount}
+      />
+
+        <AuthorNFTCardBox
+          collectiables={collectiables}
+          created={created}
+          like={like}
+          follower={follower}
+          following={following}
+          //nfts={nfts}
+          //myNFTS={myNFTs}
+        />
+      <Title
+        heading="Popular Creators"
+        paragraph="Click on music icon and enjoy NTF music or audio"
+      />
+        <div className={Style.author_box}>
+        {followerArray.map((el, i) => (
+          <FollowerTabCard i={i} el={el} />
+        ))}
+      </div>
+
+      <Brand/>
+    </div>
+  )
+}
+
+export default author
