fbac713ab968b4e5466f38cfeb35ba51673c8a11
Sa1tyPretzel <mithileshpawar1@gmail.com>
2023-03-25 18:59:08 +0530

    setup hardhat

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
 
-//INTERNAL IMPORT
+//INTRNAL IMPORT
 import { NavBar, Footer } from "../components/componentsindex";
+import { NFTMarketplaceProvider } from "../Context/NFTMarketplaceContext";
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
-    <NavBar />
-    <Component {...pageProps} />
-    <Footer />
-  </div>)
+    <NFTMarketplaceProvider>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </NFTMarketplaceProvider>
+  </div>
+);
 
 export default MyApp;
